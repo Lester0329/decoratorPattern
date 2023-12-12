076168bb37e2b31509237ce740805d34e6c9aa31
Lester0329 <72334101+Lester0329@users.noreply.github.com>
2023-12-12 22:11:06 +0800

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,39 @@
+package decoratorPattern;
+
+public class GSave implements BankAccount {
+    private BankAccount bankAccount;
+
+    public GSave(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "GSave";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.025;
+    }
+
+    @Override
+    public double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public String showBenefits() {
+        return bankAccount.showBenefits() + ",GCash Transfer";
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showInfo() {
+        return bankAccount.showInfo();
+    }
+}
